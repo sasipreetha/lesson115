5969e8a310e44421c7df3651398f59d63572e97e
sasipreetha <30656181+sasipreetha@users.noreply.github.com>
2024-08-30 00:29:28 +0530

    Add files via upload

---
+++ split.py
@@ -0,0 +1,14 @@
+# List initialisation
+list = ['Geeks', 'forgeeks', 'is a', 'portal', 'for Geeks']
+ 
+# Splitting list into first half
+first_list = list[:list.index('forgeeks')]
+ 
+# Splitting list into second half
+second_list = list[list.index('forgeeks')+1:]
+ 
+# Printing first list
+print(first_list)
+ 
+# Printing second list
+print(second_list)
